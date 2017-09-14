c9691711f6f5edeab54ecbbeabc420bcbb8a1eef
xuc <xuc@sinux.com.cn>
2017-09-14 10:58:12 +0800

    增加登录私有仓库命令

--- push-all-images.sh
+++ push-all-images.sh
@@ -4,7 +4,7 @@ set -e
 
 # Docker image prefix
 REGPREFIX=registry.cn-hangzhou.aliyuncs.com/xuc/
-
+docker login --username=daxiang6506@sina.com -p 2017XCxc registry.cn-hangzhou.aliyuncs.com
 docker push ${REGPREFIX}discovery-server
 docker push ${REGPREFIX}gateway
 docker push ${REGPREFIX}foo
