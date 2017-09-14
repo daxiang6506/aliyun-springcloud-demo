843d0c2b1a8b9913a51c58b40baa184d0233f617
xuc <xuc@sinux.com.cn>
2017-09-14 10:35:00 +0800

    修改镜像仓库位置 修改date函数参数

--- build-all.sh
+++ build-all.sh
@@ -10,39 +10,39 @@ function progress() {
 set -e
 
 # Docker image prefix
-REGPREFIX=registry.aliyuncs.com/jingshanlb/
+REGPREFIX=registry.cn-hangzhou.aliyuncs.com/xuc/
 
 progress "Building discovery-server(1/5) jar file ..."
 cd common/discovery-server
 ./gradlew build
 progress "Building discovery-server(1/5) docker image ..."
-docker tag $(docker build -t ${REGPREFIX}discovery-server -q .) ${REGPREFIX}discovery-server:$(date -ju "+%Y%m%d-%H%M%S")
+docker tag $(docker build -t ${REGPREFIX}discovery-server -q .) ${REGPREFIX}discovery-server:$(date -u "+%Y%m%d-%H%M%S")
 cd -
 
 progress "Building gateway(2/5) jar file ..."
 cd common/gateway
 ./gradlew build
 progress "Building gateway(2/5) docker image ... "
-docker tag $(docker build -t ${REGPREFIX}gateway -q .) ${REGPREFIX}gateway:$(date -ju "+%Y%m%d-%H%M%S")
+docker tag $(docker build -t ${REGPREFIX}gateway -q .) ${REGPREFIX}gateway:$(date -u "+%Y%m%d-%H%M%S")
 cd -
 
 progress "Building foo service(3/5) jar file ..."
 cd services/foo
 ./gradlew build
 progress "Building foo service(3/5) docker image ..."
-docker tag $(docker build -t ${REGPREFIX}foo -q .) ${REGPREFIX}foo:$(date -ju "+%Y%m%d-%H%M%S")
+docker tag $(docker build -t ${REGPREFIX}foo -q .) ${REGPREFIX}foo:$(date -u "+%Y%m%d-%H%M%S")
 cd -
 
 progress "Building bar service(4/5) jar file ..."
 cd services/bar
 ./gradlew build
 progress "Building bar service(4/5) docker image ..."
-docker tag $(docker build -t ${REGPREFIX}bar -q .) ${REGPREFIX}bar:$(date -ju "+%Y%m%d-%H%M%S")
+docker tag $(docker build -t ${REGPREFIX}bar -q .) ${REGPREFIX}bar:$(date -u "+%Y%m%d-%H%M%S")
 cd -
 
 progress "Building foobar service(5/5) jar file ..."
 cd services/foobar
 ./gradlew build
 progress "Building foobar service(5/5) docker image ..."
-docker tag $(docker build -t ${REGPREFIX}foobar -q .) ${REGPREFIX}foobar:$(date -ju "+%Y%m%d-%H%M%S")
+docker tag $(docker build -t ${REGPREFIX}foobar -q .) ${REGPREFIX}foobar:$(date -u "+%Y%m%d-%H%M%S")
 cd -
